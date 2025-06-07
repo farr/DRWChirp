Fixed chirp mass scaling so it's in proper geometric units.

chirp.py
@@ -15,6 +15,8 @@ import xarray as xr
 frequency_spacing_constant = 7.72525 # This is the distance to the first sideband of the likelihood in white noise for a sinusoidal signal
 frequency_derivative_spacing_constant = 15.121 # This is the distance to the first sideband of the likelihood for exp(1/2 I wdot t^2) signal
 
+chirp_mass_constant = 5 / 2**(8/5)
+
 def chirp_phase(t, tc, mc):
     r"""Returns the phase of a chirp signal at time `t`, with coalescence time `tc`
     and chirp mass `mc`.
@@ -34,7 +36,7 @@ def chirp_phase(t, tc, mc):
     mc = mc[(slice(None),)*len(mc_shape) + (jnp.newaxis,)*len(t_shape)]
     t = t[(jnp.newaxis,)*len(tc_shape) + (slice(None),)*len(t_shape)]
 
-    theta = (tc - t) / mc
+    theta = (tc - t) / (chirp_mass_constant * mc)
 
     return jnp.where(theta > 0, -theta**(5/8), 0)
 